a7c82c91369afe2d19238e2d8d6fa669bbe1c175
Raul Yesid Rojas Ballesteros <131273447+YesidRojas06@users.noreply.github.com>
2024-01-25 13:48:52 -0500

    hola

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,100 +1,100 @@
 <template>
     <div>
-        <q-layout view="hHh LpR lFf">
-
-
-            <q-header elevated class="bg-primary text-white">
-                <q-toolbar>
-                    <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-
-                    <q-toolbar-title>
-                        <q-avatar>
-                            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-                        </q-avatar>
-                        Title
-                    </q-toolbar-title>
-                </q-toolbar>
-            </q-header>
-
-
-            <q-drawer v-model="leftDrawerOpen" side="left" overlay behavior="desktop" bordered>
-                <!-- drawer content -->
-            </q-drawer>
-            <q-page-container>
-                <div class="tarjetas">
-                    <div class="card">
-                        <h2>CARD</h2>
-                    </div>
-                    <div class="card">
-                        <h2>CARD</h2>
-                    </div>
-                    <div class="card">
-                        <h2>CARD</h2>
-                    </div>
-                    <div class="card">
-                        <h2>CARD</h2>
-                    </div>
-                    <div class="card">
-                        <h2>CARD</h2>
-                    </div>
-                    <div class="card">
-                        <h2>CARD</h2>
-                    </div>
-                    <div class="card">
-                        <h2>CARD</h2>
-                    </div>
-                    <div class="card">
-                        <h2>CARD</h2>
-                    </div>
-                    <div class="card">
-                        <h2>CARD</h2>
-                    </div>
-                    <div class="card">
-                        <h2>CARD</h2>
-                    </div>
-                </div>
-
-                <router-view />
-            </q-page-container>
-        </q-layout>
-
-    </div>
-</template>
+      <q-layout view="hHh LpR fff">
+        <q-header reveal elevated class="bg-primary text-white">
+          <q-toolbar>
+            <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
   
-<script  setup >
-import { ref } from 'vue'
-
-
-let leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer() {
-    leftDrawerOpen.value = !leftDrawerOpen.value
-}
-
-
-</script>
-
-<style scoped>
-.q-page-container {
+            <q-toolbar-title>
+              <q-avatar>
+                <img
+                  src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg"
+                />
+              </q-avatar>
+              Inicio
+            </q-toolbar-title>
+          </q-toolbar>
+        </q-header>
+  
+        <q-drawer
+          show-if-above
+          v-model="leftDrawerOpen"
+          side="left"
+          behavior="desktop"
+          elevated
+        >
+          <!-- drawer content -->
+        </q-drawer>
+        <q-page-container>
+          <div class="tarjetas">
+            <div class="card">
+              <h2>CARD</h2>
+            </div>
+            <div class="card">
+              <h2>CARD</h2>
+            </div>
+            <div class="card">
+              <h2>CARD</h2>
+            </div>
+            <div class="card">
+              <h2>CARD</h2>
+            </div>
+            <div class="card">
+              <h2>CARD</h2>
+            </div>
+            <div class="card">
+              <h2>CARD</h2>
+            </div>
+            <div class="card">
+              <h2>CARD</h2>
+            </div>
+            <div class="card">
+              <h2>CARD</h2>
+            </div>
+            <div class="card">
+              <h2>CARD</h2>
+            </div>
+            <div class="card">
+              <h2>CARD</h2>
+            </div>
+          </div>
+  
+          <router-view />
+        </q-page-container>
+      </q-layout>
+    </div>
+  </template>
+    
+  <script setup>
+  import { ref } from 'vue';
+  
+  const leftDrawerOpen = ref(false);
+  
+  const toggleLeftDrawer = () => {
+    leftDrawerOpen.value = !leftDrawerOpen.value;
+  };
+  </script>
+  
+  <style scoped>
+  .q-page-container {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
-}
-
-.tarjetas {
+  }
+  
+  .tarjetas {
     display: flex;
     flex-direction: row;
     gap: 80px;
-    flex-wrap:wrap ;
+    flex-wrap: wrap;
     align-items: center;
     justify-content: center;
-
-}
-
-.card {
+  }
+  
+  .card {
     width: 190px;
     height: 254px;
-    background: #07182E;
+    background: #07182e;
     position: relative;
     display: flex;
     flex-direction: column;
@@ -104,40 +104,43 @@ function toggleLeftDrawer() {
     border-radius: 20px;
     margin: 10px;
     /* Espaciado entre tarjetas */
-}
-
-.card h2 {
+  }
+  
+  .card h2 {
     z-index: 1;
     color: white;
     font-size: 2em;
-}
-
-.card::before {
-    content: '';
+  }
+  
+  .card::before {
+    content: "";
     position: absolute;
     width: 100px;
-    background-image: linear-gradient(180deg, rgb(0, 183, 255), rgb(255, 48, 255));
+    background-image: linear-gradient(
+      180deg,
+      rgb(0, 183, 255),
+      rgb(255, 48, 255)
+    );
     height: 130%;
     animation: rotBGimg 3s linear infinite;
     transition: all 0.2s linear;
-}
-
-@keyframes rotBGimg {
+  }
+  
+  @keyframes rotBGimg {
     from {
-        transform: rotate(0deg);
+      transform: rotate(0deg);
     }
-
+  
     to {
-        transform: rotate(360deg);
+      transform: rotate(360deg);
     }
-}
-
-.card::after {
-    content: '';
+  }
+  
+  .card::after {
+    content: "";
     position: absolute;
-    background: #07182E;
+    background: #07182e;
     inset: 5px;
     border-radius: 15px;
-}
-</style>
-
+  }
+  </style>  
