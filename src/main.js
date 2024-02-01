d9a994fa0addcf43465cc412acef53624bbdbf8b
AngelB10 <131167121+AngelB10@users.noreply.github.com>
2024-02-01 14:13:33 -0500

    a

--- src/main.js
+++ src/main.js
@@ -2,19 +2,27 @@
 // import './style.css'
 import App from './App.vue'
 import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 import {router} from "./routes/routes.js"
 import { Quasar } from 'quasar'
+import axios from 'axios'
 
 import '@quasar/extras/material-icons/material-icons.css'
 
 import 'quasar/src/css/index.sass'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(Quasar, {
-  plugins: {},
+    plugins: {
+        Notify,
+    }
 })
 
+app.use(pinia)
+app.use(router)
+axios.deafaulst.baseURL = 'https://backend-abxx.onrender.com/api/'
 
-app.use (router)
+
 app.mount('#app')
